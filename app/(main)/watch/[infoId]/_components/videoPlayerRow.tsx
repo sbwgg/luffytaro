4ffb36880932f5011376de65aa3a4752085649e9
julz <owanojulius742002@gmail.com>
2024-01-26 14:52:56 +0800

    Remove videoplayer

--- app/(main)/watch/[infoId]/_components/videoPlayerRow.tsx
+++ app/(main)/watch/[infoId]/_components/videoPlayerRow.tsx
@@ -24,7 +24,6 @@ import { useRouter } from "next/navigation";
 import { TbFaceIdError } from "react-icons/tb";
 import { AnimeInfoType, MetaAnilistInfoType } from "@/app/(main)/[infoId]/page";
 import NextEpisodeTime from "@/components/nextEpisodeTime";
-import { Poster } from "@vidstack/react";
 
 interface VideoPlayerRowProp {
   episodeServer: EpisodeServerType;
@@ -136,128 +135,125 @@ const VideoPlayerRow = ({
             <p className="text-zinc-700 text-xl">Please wait...</p>
           </div>
         ) : (
-          <MediaPlayer
-            playsinline={true}
-            aspectRatio="16/9"
-            ref={playerRef}
-            key={episodeServer.episodeId}
-            title={animeInfo.anime.info.name}
-            src={defaultUrl?.url || ""}
-            onEnd={() => {
-              setWatchedTime((prev) => prev.filter((item) => item.ep !== ep));
+          <>
+            {/*  <MediaPlayer
+              playsinline={true}
+              aspectRatio="16/9"
+              ref={playerRef}
+              key={episodeServer.episodeId}
+              title={animeInfo.anime.info.name}
+              src={defaultUrl?.url || ""}
+              onEnd={() => {
+                setWatchedTime((prev) => prev.filter((item) => item.ep !== ep));
 
-              if (autonext) {
-                router.push(`/watch/${nextEp.episodeId}`);
-              }
-            }}
-            autoplay={autoplay}
-            onStarted={() => {
-              if (storedWatchTime?.currentTime) {
-                playerRef.current!.currentTime = storedWatchTime?.currentTime;
-              }
-            }}
-            onTimeUpdate={(detail) => {
-              const currentTime = detail.currentTime;
-
-              if (
-                currentSkiptime &&
-                currentTime >= currentSkiptime.intro.start &&
-                currentTime <= currentSkiptime.intro.end
-              ) {
-                if (autoskipIntro) {
-                  playerRef.current!.currentTime = currentSkiptime.intro.end;
-                } else {
-                  setShowSkipbutton((prev) => ({ ...prev, intro: true }));
+                if (autonext) {
+                  router.push(`/watch/${nextEp.episodeId}`);
                 }
-              } else {
-                setShowSkipbutton((prev) => ({ ...prev, intro: false }));
-              }
-
-              if (
-                currentSkiptime &&
-                currentTime >= currentSkiptime.outro.start &&
-                currentTime <= currentSkiptime.outro.end
-              ) {
-                if (autoskipIntro) {
-                  playerRef.current!.currentTime = currentSkiptime.outro.end;
-                } else {
-                  setShowSkipbutton((prev) => ({ ...prev, outro: true }));
+              }}
+              autoplay={autoplay}
+              onStarted={() => {
+                if (storedWatchTime?.currentTime) {
+                  playerRef.current!.currentTime = storedWatchTime?.currentTime;
                 }
-              } else {
-                setShowSkipbutton((prev) => ({ ...prev, outro: false }));
-              }
+              }}
+              onTimeUpdate={(detail) => {
+                const currentTime = detail.currentTime;
 
-              if (currentTime > 1) {
-                setWatchedTime((prev) =>
-                  !prev.some((item) => item.ep === ep)
-                    ? [
-                        ...prev,
-                        {
-                          currentTime: Math.floor(currentTime),
-                          endTime: Math.floor(duration),
-                          episodeNo: episodeServer.episodeNo,
-                          infoId,
-                          ep,
-                          lastViewed: true,
-                        },
-                      ]
-                    : prev.map((item) =>
-                        item.ep === ep
-                          ? {
-                              ...item,
-                              currentTime: Math.floor(currentTime),
-                              lastViewed: true,
-                              endTime: Math.floor(duration),
-                            }
-                          : item.ep !== ep && item.infoId === infoId
-                          ? { ...item, lastViewed: false }
-                          : item
-                      )
-                );
-              }
-            }}
-          >
-            <MediaProvider>
-              {/*   <Poster
-                className="absolute inset-0 block h-full w-full bg-black rounded-md opacity-0 transition-opacity data-[visible]:opacity-100 [&>img]:h-full [&>img]:w-full [&>img]:object-cover"
-                src={animeInfo.anime.info.poster}
-                alt="Girl walks into campfire with gnomes surrounding her friend ready for their next meal!"
-              /> */}
-              {streamingLink?.subtitles
-                ?.filter((sub) => sub.lang !== "Thumbnails")
-                .map((sub, i) => (
-                  <Fragment key={i}>
-                    <Track
-                      src={sub.url}
-                      kind="subtitles"
-                      label={sub.lang}
-                      lang={sub.lang}
-                      default={sub.lang === "English"}
-                    />
-                  </Fragment>
-                ))}
-            </MediaProvider>
-            {showSkipbutton?.intro || showSkipbutton?.outro ? (
-              <button
-                onClick={() => {
-                  currentSkiptime
-                    ? showSkipbutton.intro
-                      ? (playerRef.current!.currentTime =
-                          currentSkiptime?.intro.end)
-                      : (playerRef.current!.currentTime =
-                          currentSkiptime?.outro.end)
-                    : null;
-                }}
-                className="absolute bottom-[4.8rem] sm:bottom-[5.5rem] left-[.7rem] sm:left-5 sm:text-base text-sm p-1 px-3 bg-white text-black z-[100]"
-              >
-                Skip {showSkipbutton.intro ? "Intro" : "Outro"}
-              </button>
-            ) : null}
-            <DefaultVideoLayout
-              thumbnails={thumbnails?.url}
-              icons={defaultLayoutIcons}
-            />
-          </MediaPlayer>
+                if (
+                  currentSkiptime &&
+                  currentTime >= currentSkiptime.intro.start &&
+                  currentTime <= currentSkiptime.intro.end
+                ) {
+                  if (autoskipIntro) {
+                    playerRef.current!.currentTime = currentSkiptime.intro.end;
+                  } else {
+                    setShowSkipbutton((prev) => ({ ...prev, intro: true }));
+                  }
+                } else {
+                  setShowSkipbutton((prev) => ({ ...prev, intro: false }));
+                }
+
+                if (
+                  currentSkiptime &&
+                  currentTime >= currentSkiptime.outro.start &&
+                  currentTime <= currentSkiptime.outro.end
+                ) {
+                  if (autoskipIntro) {
+                    playerRef.current!.currentTime = currentSkiptime.outro.end;
+                  } else {
+                    setShowSkipbutton((prev) => ({ ...prev, outro: true }));
+                  }
+                } else {
+                  setShowSkipbutton((prev) => ({ ...prev, outro: false }));
+                }
+
+                if (currentTime > 1) {
+                  setWatchedTime((prev) =>
+                    !prev.some((item) => item.ep === ep)
+                      ? [
+                          ...prev,
+                          {
+                            currentTime: Math.floor(currentTime),
+                            endTime: Math.floor(duration),
+                            episodeNo: episodeServer.episodeNo,
+                            infoId,
+                            ep,
+                            lastViewed: true,
+                          },
+                        ]
+                      : prev.map((item) =>
+                          item.ep === ep
+                            ? {
+                                ...item,
+                                currentTime: Math.floor(currentTime),
+                                lastViewed: true,
+                                endTime: Math.floor(duration),
+                              }
+                            : item.ep !== ep && item.infoId === infoId
+                            ? { ...item, lastViewed: false }
+                            : item
+                        )
+                  );
+                }
+              }}
+            >
+              <MediaProvider>
+                {streamingLink?.subtitles
+                  ?.filter((sub) => sub.lang !== "Thumbnails")
+                  .map((sub, i) => (
+                    <Fragment key={i}>
+                      <Track
+                        src={sub.url}
+                        kind="subtitles"
+                        label={sub.lang}
+                        lang={sub.lang}
+                        default={sub.lang === "English"}
+                      />
+                    </Fragment>
+                  ))}
+              </MediaProvider>
+              {showSkipbutton?.intro || showSkipbutton?.outro ? (
+                <button
+                  onClick={() => {
+                    currentSkiptime
+                      ? showSkipbutton.intro
+                        ? (playerRef.current!.currentTime =
+                            currentSkiptime?.intro.end)
+                        : (playerRef.current!.currentTime =
+                            currentSkiptime?.outro.end)
+                      : null;
+                  }}
+                  className="absolute bottom-[4.8rem] sm:bottom-[5.5rem] left-[.7rem] sm:left-5 sm:text-base text-sm p-1 px-3 bg-white text-black z-[100]"
+                >
+                  Skip {showSkipbutton.intro ? "Intro" : "Outro"}
+                </button>
+              ) : null}
+              <DefaultVideoLayout
+                thumbnails={thumbnails?.url}
+                icons={defaultLayoutIcons}
+              />
+            </MediaPlayer> */}
+          </>
         )}
       </div>
 
