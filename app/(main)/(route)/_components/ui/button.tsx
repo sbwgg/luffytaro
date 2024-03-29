633f9b046bad465c84b788659c3e5077d5fcbff7
Vrwi <82095317+sbwgg@users.noreply.github.com>
2024-03-29 23:39:12 +0000

    bug

--- app/(main)/(route)/_components/ui/button.tsx
+++ app/(main)/(route)/_components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default: "bg-zinc-700/40 text-primary-foreground hover:bg-zinc-700/40",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
