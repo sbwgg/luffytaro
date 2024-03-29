1b167fa708ce4076d61343b20c4fda80e91dd8bb
Vrwi <82095317+sbwgg@users.noreply.github.com>
2024-03-29 19:53:39 +0000

    fix1

--- app/(main)/(route)/_components/estimatedSchedule.tsx
+++ app/(main)/(route)/_components/estimatedSchedule.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useEffect, useRef, useState } from "react";
 import { Swiper, SwiperRef, SwiperSlide } from "swiper/react";
 import { TiChevronRight, TiChevronLeft } from "react-icons/ti";
