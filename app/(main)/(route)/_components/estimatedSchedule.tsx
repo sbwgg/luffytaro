a48d7c51a29a5984f8a0fd0a820240a0c07d3a86
Vrwi <82095317+sbwgg@users.noreply.github.com>
2024-03-30 00:07:23 +0000

    fix color

--- app/(main)/(route)/_components/estimatedSchedule.tsx
+++ app/(main)/(route)/_components/estimatedSchedule.tsx
@@ -178,7 +178,7 @@ const ScheduleAnime = () => {
                 <Button
                   className={cn(
                     "w-48 rounded-none mr-6 h-12",
-                    isActive && "bg-red-600"
+                    isActive && "bg-red-500"
                   )}
                   variant="secondary"
                   key={index}
