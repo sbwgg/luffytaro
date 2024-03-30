f972ee7634a6b758ce8a002582d423d9fe543e63
Vrwi <82095317+sbwgg@users.noreply.github.com>
2024-03-30 15:33:06 +0000

    use client for search page

--- app/(main)/search/page.tsx
+++ app/(main)/search/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useState, useEffect } from "react";
 import GridCardAnime from "@/components/gridCardAnime";
 import MostPopularAnime from "@/components/mostPopularAnime";
