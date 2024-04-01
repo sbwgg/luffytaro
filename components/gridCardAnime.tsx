69c1013fce25540b9b1c5a3e99353656ec5c24e4
Vrwi <82095317+sbwgg@users.noreply.github.com>
2024-04-01 21:35:01 +0000

    poster sie fix

--- components/gridCardAnime.tsx
+++ components/gridCardAnime.tsx
@@ -1,8 +1,6 @@
-"use client";
-
 import Image from "next/image";
-import React, { useState, useEffect } from "react";
-import { FaClosedCaptioning } from "react-icons/fa";
+import React from "react";
+import { FaClosedCaptioning } from "react-icons/fa6";
 import { FaMicrophone } from "react-icons/fa";
 import Link from "next/link";
 import { AnimeInfoType } from "@/app/(main)/[infoId]/page";
@@ -22,72 +20,57 @@ interface LatestEpisodeProp {
   };
 }
 
-const GridCardAnime = ({ anime }: LatestEpisodeProp) => {
-  const [showInfo, setShowInfo] = useState(false);
-  const [animeInfo, setAnimeInfo] = useState<AnimeInfoType | null>(null);
-
-  useEffect(() => {
-    const fetchAnimeInfo = async () => {
-      const fetchedInfo: AnimeInfoType = await fetch(
-        `${process.env.ANIWATCH_URL}/anime/info?id=${anime.id}`
-      ).then((res) => res.json());
-      setAnimeInfo(fetchedInfo);
-    };
-
-    if (showInfo) {
-      fetchAnimeInfo();
+const GridCardAnime = async ({ anime }: LatestEpisodeProp) => {
+  const animeInfo: AnimeInfoType = await fetch(
+    `${process.env.ANIWATCH_URL}/anime/info?id=${anime.id}`,
+    {
+      next: {
+        revalidate: 60,
+      },
     }
-  }, [anime.id, showInfo]);
+  ).then((res) => res.json());
 
   return (
-    <div className="relative w-full">
-      <div
-        className="relative w-full sm:h-[16rem] h-[14rem] cursor-pointer"
-        onMouseEnter={() => setShowInfo(true)}
-        onMouseLeave={() => setShowInfo(false)}
-      >
-        <span className="absolute inset-0 bg-gradient-to-t from-black/90 via-transparent to-transparent" />
-        {anime.rating && (
-          <p className="absolute right-1 top-1 text-xs p-1 px-2 bg-red-500 rounded">
-            {anime.rating}
-          </p>
-        )}
-        <div className="flex items-center gap-x-2 absolute bottom-2 left-2">
-          {anime.episodes.sub && (
-            <p className="flex items-center gap-x-1 text-xs">
-              <FaClosedCaptioning /> {anime.episodes.sub}
+    <div>
+      <Link href={`/${anime.id}`}>
+        <div className="relative w-full sm:h-[16rem] h-[14rem]">
+          <span className="absolute inset-0 bg-gradient-to-t from-black/90 via-transparent to-transparent" />
+          {anime.rating && (
+            <p className="absolute right-1 top-1 text-xs p-1 px-2 bg-red-500 rounded">
+              {anime.rating}
             </p>
           )}
-          {anime.episodes.dub && (
-            <p className="flex items-center gap-x-1 text-xs">
-              <FaMicrophone /> {anime.episodes.dub}
-            </p>
-          )}
+          <div className="flex items-center gap-x-2 absolute bottom-2 left-2">
+            {anime.episodes.sub && (
+              <p className="flex items-center gap-x-1 text-xs">
+                <FaClosedCaptioning /> {anime.episodes.sub}
+              </p>
+            )}
+            {anime.episodes.dub && (
+              <p className="flex items-center gap-x-1 text-xs">
+                <FaMicrophone /> {anime.episodes.dub}
+              </p>
+            )}
+          </div>
+          <Image
+            src={anime.poster}
+            alt="poster"
+            width={400}
+            height={300}
+            priority
+            className="w-full h-full object-cover shrink-0"
+          />
         </div>
-        <Image
-          src={anime.poster}
-          alt="poster"
-          width={400}
-          height={300}
-          priority
-          className="w-full h-full object-cover shrink-0"
-        />
-      </div>
-      {showInfo && animeInfo && (
-        <div className="absolute top-full left-0 bg-black text-white p-4">
-          <h3>{anime.name}</h3>
-          <p>{animeInfo.anime.info.description}</p>
-          <Link href={`/${anime.id}`}>
-            <button className="mt-2 bg-red-500 text-white px-4 py-2 rounded hover:bg-red-600">
-              Watch Now
-            </button>
-          </Link>
-        </div>
-      )}
+      </Link>
       <div className="mt-2">
         <p className="truncate hover:text-red-500 text-sm">
           <Link href={`/${anime.id}`}>{anime.name}</Link>
         </p>
+        {animeInfo?.anime?.info?.description && (
+          <p className="ellip my-2 text-[13px] text-zinc-400">
+            {animeInfo?.anime?.info?.description}
+          </p>
+        )}
         <p className="flex items-center gap-x-2 text-zinc-400 mt-2 text-xs">
           <span className="truncate">{anime.duration}</span> &#x2022;{" "}
           <span className="truncate">{anime.type}</span>
