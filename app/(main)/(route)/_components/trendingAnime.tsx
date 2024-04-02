071f91795b410c315eddd6922178404db11c2479
Vrwi <82095317+sbwgg@users.noreply.github.com>
2024-04-02 14:50:16 +0000

    outline font

--- app/(main)/(route)/_components/trendingAnime.tsx
+++ app/(main)/(route)/_components/trendingAnime.tsx
@@ -51,16 +51,22 @@ const TrendingAnime = ({ trending }: TrendingAnimeProp) => {
         spaceBetween={8}
         ref={swiperRef}
       >
-        {trending.map((trend) => (
+        {trending.map((trend, index) => (
           <SwiperSlide key={trend.id}>
             <Link href={`/${trend.id}`}>
               <div className="relative mb-2 overflow-hidden">
                 <div
                   className={cn(
-                    "absolute left-0 bg-white text-black p-1 px-3 flex justify-center z-[10]"
+                    "absolute left-0 bg-white text-black p-1 px-3 flex justify-center z-[10]",
+                    // Apply red, orange, yellow color to the first three items
+                    index < 3 ? `text-${['red', 'orange', 'yellow'][index]}` : ''
                   )}
+                  style={{ // Add outline to the text
+                    WebkitTextStroke: "1px black",
+                    textShadow: "2px 2px 4px rgba(0, 0, 0, 0.3)"
+                  }}
                 >
-                  <p className="">#{trend.rank}</p>
+                  <p className="">{`#${trend.rank}`}</p>
                 </div>
                 <Image
                   src={trend.poster}
